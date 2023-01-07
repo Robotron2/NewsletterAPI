28bd9dca72cf18b0da60ee53a8b06049ec0de294
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-07 16:04:29 +0100

    signUp.html styled completely

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express")
+const request = require("request")
 const bodyParser = require("body-parser")
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//Use static files
+app.use(express.static("public"))
+
 app.get("/", function (req, res) {
-	res.send("Hello, server is running")
+	res.sendFile(__dirname + "/signup.html")
 })
 
 app.listen(3000, function () {
